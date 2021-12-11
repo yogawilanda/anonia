b68045e786e70ef30e65a3fadc93a148c886e9a4
Tesla <hyoga.wilanda@gmail.com>
2021-12-11 18:21:34 +0700

    0.0.4 google signin

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,23 @@
 // import 'package:anonia/route/route.dart';
 // import 'package:anonia/ui/login.dart';
 // import 'package:anonia/ui/register.dart';
+import 'package:anonia/google_sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'route/route.dart' as route;
 // import 'search_screen.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:anonia/settings_page.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const AnoniaApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const AnoniaApp());
+}
 
 class AnoniaApp extends StatefulWidget {
   const AnoniaApp({Key? key}) : super(key: key);
@@ -23,17 +31,18 @@ class AnoniaApp extends StatefulWidget {
 
 class AnoniaAppState extends State<AnoniaApp> {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Anonia',
-      // home: SearchScreen(),
-      initialRoute: '/',
-      onGenerateRoute: route.getRoute,
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => GoogleSignInProvider(),
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Anonia',
+          // home: SearchScreen(),
+          initialRoute: '/',
+          onGenerateRoute: route.getRoute,
 
-      // TODO: Pindahkan ThemeData ini ke theme data file terpisah
+          // TODO: Pindahkan ThemeData ini ke theme data file terpisah
 
-      // TODO: Buat routing setelah pembuatan login interface selesai
-    );
-  }
+          // TODO: Buat routing setelah pembuatan login interface selesai
+        ),
+      );
 }
