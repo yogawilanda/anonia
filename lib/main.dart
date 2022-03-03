6c490a14698bb80f0fe0b76173a968274f225d54
yogawilanda <hyoga.wilanda@gmail.com>
2022-03-03 14:11:37 +0700

    modify to resolve bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:anonia/google_logged_in_widget.dart';
+import 'package:anonia/appGate.dart';
 import 'package:anonia/authenticator.dart';
 import 'package:flutter/material.dart';
 import 'route/route.dart' as route;
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'authenticator.dart';
@@ -19,9 +18,6 @@ class AnoniaApp extends StatelessWidget {
   static const String title = 'Anonia';
   @override
   Widget build(BuildContext context) {
-    bool isInsider = false;
-    final user = FirebaseAuth.instance.currentUser;
-
     return ChangeNotifierProvider(
       create: (context) => GoogleSignInProvider(),
       child: const MaterialApp(
