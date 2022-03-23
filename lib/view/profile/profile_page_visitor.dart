2077982caf06ab18404c536ad9bdc1ada593590d
yogawilanda <hyoga.wilanda@gmail.com>
2022-03-24 00:38:43 +0700

    working on the profile page

---
+++ lib/view/profile/profile_page_visitor.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProfilePageVisitor extends StatelessWidget {
+  const ProfilePageVisitor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        DraggableScrollableSheet(
+          initialChildSize: 0.25,
+          maxChildSize: 0.6,
+          minChildSize: 0.1,
+          snap: true,
+          builder: (context, scrollController) {
+            return Container(
+              decoration: boxCustomDecoration(),
+              child: ListView.builder(
+                itemBuilder: (context, i) {
+                  return Container(
+                    child: Center(
+                        child: Column(
+                      children: [],
+                    )),
+                  );
+                },
+              ),
+            );
+          },
+        )
+      ]),
+    );
+  }
+
+  boxCustomDecoration() {
+    return BoxDecoration(
+      color: Colors.blue,
+      borderRadius: BorderRadiusDirectional.only(
+        topEnd: Radius.circular(60),
+        topStart: Radius.circular(60),
+      ),
+    );
+  }
+}
