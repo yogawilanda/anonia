63a7d5264a4fc9abe00fa84493e984224a145dcb
yogawilanda <hyoga.wilanda@gmail.com>
2022-03-24 04:42:21 +0700

    working on profile page background image

--- lib/view/profile/profile_page_visitor.dart
+++ lib/view/profile/profile_page_visitor.dart
@@ -7,39 +7,62 @@ class ProfilePageVisitor extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-      body: Stack(children: [
-        // DraggableScrollableSheet(
-        //   initialChildSize: 0.25,
-        //   maxChildSize: 0.6,
-        //   minChildSize: 0.1,
-        //   snap: true,
-        //   builder: (context, scrollController) {
-        //     return Container(
-        //       decoration: boxCustomDecoration(),
-        //       child: ListView.builder(
-        //         itemBuilder: (context, i) {
-        //           return Container(
-        //             child: Center(
-        //                 child: Column(
-        //               children: [],
-        //             )),
-        //           );
-        //         },
-        //       ),
-        //     );
-        //   },
-        // )
-      ]),
-    );
-  }
+      body: Stack(
+        children: [
+          Dragsheet(),
 
-  boxCustomDecoration() {
-    return BoxDecoration(
-      color: Colors.blue,
-      borderRadius: BorderRadiusDirectional.only(
-        topEnd: Radius.circular(60),
-        topStart: Radius.circular(60),
+          //background image asset
+          Container(
+            child: Image.asset(
+              //Todo: add background image as an example of how image background look like.
+              '',
+              fit: BoxFit.cover,
+            ),
+            alignment: Alignment.center,
+          ),
+        ],
       ),
     );
   }
+}
+
+class Dragsheet extends StatelessWidget {
+  const Dragsheet({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.25,
+      maxChildSize: 0.6,
+      minChildSize: 0.1,
+      snap: true,
+      builder: (context, scrollController) {
+        return Container(
+          decoration: boxCustomDecoration(),
+          child: ListView.builder(
+            itemBuilder: (context, i) {
+              return Container(
+                child: Center(
+                    child: Column(
+                  children: [],
+                )),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+boxCustomDecoration() {
+  return BoxDecoration(
+    color: Colors.blue,
+    borderRadius: BorderRadiusDirectional.only(
+      topEnd: Radius.circular(60),
+      topStart: Radius.circular(60),
+    ),
+  );
 }
